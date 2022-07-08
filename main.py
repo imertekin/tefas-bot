4c5c7031be18bf24cd2b6d8e940610c397b8bfeb
imertekin <iertekinx@gmail.com>
2022-07-08 11:41:04 +0300

    Add discord command

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord
+from discord.ext import commands
 from api import get_fon
 from dotenv import load_dotenv
 import os
@@ -11,26 +11,40 @@ with open('tefas_kod.csv', 'r') as f:
         words.append(line.split(","))
     words=words[0]
 
-client = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix='$')
+
+
+@bot.command()
+async def fon(ctx, *args):
+    for i in args:
+        i = i.upper()
+        if i in words:
+            result = get_fon([i])
+        await ctx.send(f'```json\n{result}\n```')
+
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
+    mention = message.author.mention
+    msg = message.content.upper()
 
-
-    msg = message.content
-
-    if msg.startswith('$'):
-        kod = msg.split('$')[1]
-        if kod in words:
-            result = get_fon([kod])
+    if msg.startswith('~'):
+        code = msg.split('~')[1]
+        code = code.upper()
+        if code == 'ANAN' or 'ANNEN' or 'ANA' or 'BACI' or 'BACIN' or 'KARDES' or 'TEYZEN' or 'KARIN' or 'KIZIN':
+             await message.channel.send(f'{mention} senin {code}')
+        elif code in words:
+            result = get_fon([code])
             await message.channel.send(f'```json\n{result}\n```')
         else:
             await message.channel.send('Aradiginiz fon bulunamadi')
+    await bot.process_commands(message)
 
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
